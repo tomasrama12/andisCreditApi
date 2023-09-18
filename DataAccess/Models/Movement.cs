8639adf47e32c4e167e90809afbe6f145430557e
Agustin09810 <a.lorenzogon2@gmail.com>
2023-09-18 18:08:09 -0300

    Fixed error

--- DataAccess/Models/Movement.cs
+++ DataAccess/Models/Movement.cs
@@ -13,13 +13,14 @@ namespace CreditCardApi.DataAccess.Models
     public class Movement : IMovement
     {
         [Key]
+        public long movementId {get; set;}
         public float Amount {get; set;}
         public DateTime Date {get; set;}
         public string Description {get; set;}
         public string BusinessName {get; set;}
         public string currency {get; set;}
         public long cardId {get; set;}
-        public long movementId {get; set;}
+        
 
         public Movement(float amount, DateTime date, string description, string businessName, string currency, long cardId, long movementId)
         {
